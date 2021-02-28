e99701bf269914765aab892fba0aa9b0f7e51c99
Minsu Kim <alstn2468_@naver.com>
2021-02-28 23:58:30 +0900

    [minsu-210228] 2021-02-28 스터디 내용 추가 (#5)
    * 2021년 2월 28일 1.2.5까지 정리본 추가

---
+++ alstn2468/chapter1/code/code-1-10.js
@@ -0,0 +1,47 @@
+function filter(list, predicate) {
+  var new_list = [];
+  for (var i = 0, len = list.length; i < len; i++) {
+    if (predicate[list[i]]) new_list.push(list[i]);
+  }
+  return new_list;
+}
+
+function map(list, iteratee) {
+  var new_list = [];
+  for (var i = 0, len = list.length; i < len; i++) {
+    new_list.push(iteratee(list[i]));
+  }
+  return new_list;
+}
+
+var users = [
+  { id: 1, name: 'ID', age: 32 },
+  { id: 2, name: 'HA', age: 25 },
+  { id: 3, name: 'BJ', age: 32 },
+  { id: 4, name: 'PJ', age: 28 },
+  { id: 5, name: 'JE', age: 27 },
+  { id: 6, name: 'JM', age: 32 },
+  { id: 7, name: 'HI', age: 24 },
+];
+
+var ages = map(
+  filter(users, function (user) {
+    return user.age < 30;
+  }),
+  function (user) {
+    return user.age;
+  }
+);
+console.log(ages.length); // 4
+console.log(ages); // [25, 28, 27, 24]
+
+var names = map(
+  filter(users, function (user) {
+    return user.age >= 30;
+  }),
+  function (user) {
+    return user.name;
+  }
+);
+console.log(names.length); // 3
+console.log(names); // ["ID", "BJ", "JM"]
