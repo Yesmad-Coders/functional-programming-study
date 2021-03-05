b68f398a55f070bf5809dceea943cf3505b08592
alstn2468 <alstn2468_@naver.com>
2021-03-05 23:26:15 +0900

    아무리 생각해도 너무 길어져서 세부 챕터 별로 리드미 또 나눔..

---
+++ alstn2468/chapter1/code/code-1-43.js
@@ -0,0 +1,9 @@
+function f4() {
+  var a = 10;
+  var b = 20;
+  function f5() {
+    return a + b;
+  }
+  return f5;
+}
+f4(); // 30
---
+++ alstn2468/chapter1/code/code-1-45.js
@@ -0,0 +1,10 @@
+function f9() {
+  var a = 10;
+  var f10 = function (c) {
+    return a + b + c;
+  };
+  var b = 20;
+  return f10;
+}
+var f11 = f9();
+f11(30); // 60
