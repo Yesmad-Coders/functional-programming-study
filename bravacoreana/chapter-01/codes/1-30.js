9a7e237f3cb1cfc14fb0a397760777ce8c8df490
Liha <bravacoreana@gmail.com>
2021-03-12 18:03:34 +0100

    [liha-210312] done by 1.3.6

---
+++ bravacoreana/chapter-01/codes/1-30.js
@@ -0,0 +1,13 @@
+const _ = require("./data/underscore");
+
+console.log(
+  _.filter([1, 2, 3, 4], function (val, idx) {
+    return idx > 1;
+  })
+); // [3,4]
+
+console.log(
+  _.filter([1, 2, 3, 4], function (val, idx) {
+    return idx % 2 == 0;
+  })
+); // [1,3]
---
+++ bravacoreana/chapter-01/codes/1-35.js
@@ -0,0 +1,15 @@
+const _ = require("./data/underscore");
+const { not, beq } = require("./data/beq");
+
+_.some = function (list) {
+  return !!_.find(list, _.identity);
+};
+
+_.every = function (list) {
+  return beq(-1)(_.findIndex(list, not));
+};
+
+console.log(_.some([0, null, 2])); // true
+console.log(_.some([0, null, false])); // false
+console.log(_.every([0, null, 2])); // false
+console.log(_.every([{}, true, 2])); // true
