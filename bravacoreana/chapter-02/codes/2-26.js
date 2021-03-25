1c4a55e1046d9abccae6cf5d04db06fa30b1884d
Liha <bravacoreana@gmail.com>
2021-03-25 01:31:13 +0100

    [liha-210325] - code 2.28

---
+++ bravacoreana/chapter-02/codes/2-26.js
@@ -0,0 +1,10 @@
+var pj = new (function () {
+  this.name = "PJ";
+  this.age = 28;
+  this.constructor.prototype.hi = function () {
+    console.log("hi");
+  };
+})();
+
+console.log(pj); // { name: 'PJ', age: 28 }
+pj.hi(); // hi
