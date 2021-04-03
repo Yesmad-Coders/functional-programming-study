81c37c33034c0c0d6fa98cb8c66c16dbc3290f1e
devgony <devgony@gmail.com>
2021-04-03 15:46:02 +0900

    ~HTML로 출력하기+stevyQ3

--- henry/lib/fx.js
+++ henry/lib/fx.js
@@ -1,14 +1,12 @@
 const { log } = console;
 
-const products = [
-  { name: "반팔티", price: 15000, quantity: 1 },
-  { name: "긴팔티", price: 20000, quantity: 2 },
-  { name: "핸드폰케이스", price: 15000, quantity: 3 },
-  { name: "후드티", price: 30000, quantity: 4 },
-  { name: "바지", price: 25000, quantity: 5 },
-];
-
-const add = (a, b) => a + b;
+// const products = [
+//   { name: "반팔티", price: 15000, quantity: 1, is_selected: false },
+//   { name: "긴팔티", price: 20000, quantity: 2, is_selected: false },
+//   { name: "핸드폰케이스", price: 15000, quantity: 3, is_selected: false },
+//   { name: "후드티", price: 30000, quantity: 4, is_selected: false },
+//   { name: "바지", price: 25000, quantity: 5, is_selected: false },
+// ];
 
 const curry = (f) => (a, ..._) =>
   _.length ? f(a, ..._) : (..._) => f(a, ..._);
