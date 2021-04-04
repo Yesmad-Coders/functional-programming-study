f4410ba79af48d5b85d0a388addaab7817a2f9fd
Minsu Kim <alstn2468_@naver.com>
2021-04-04 22:27:15 +0900

    [minsu-210404] section3 남은 코드 추가 (#82)

--- alstn2468/section3/src/5.js
+++ alstn2468/section3/src/5.js
@@ -1,2 +1,39 @@
 import 'regenerator-runtime/runtime';
 import log from '../../lib/log';
+
+// reduce
+// 값을 하나로 축약하는 함수
+const nums = [1, 2, 3, 4, 5];
+let total = 0;
+for (const n of nums) {
+  total = total + n;
+}
+log(total); // 15
+
+/**
+ * [reduce 함수]
+ * @param {function} f    [위임할 보조함수]
+ * @param {any} [acc]     [초기값]
+ * @param {Iterable} iter [이터레이터]
+ */
+const reduce = (f, acc, iter) => {
+  if (!iter) {
+    // acc가 없을 경우 iter가 acc자리에 들어오게 된다.
+    // 따라서 acc의 이터레이터를 실행해 iter에 넣고
+    // 이러테이터의 next 메서드로 첫 값을 acc에 넣는다.
+    iter = acc[Symbol.iterator]();
+    acc = iter.next().value;
+  }
+  for (const a of iter) {
+    acc = f(acc, a);
+  }
+  return acc;
+};
+
+const add = (a, b) => a + b;
+log(reduce(add, 0, [1, 2, 3, 4, 5])); // 15
+
+// 위의 reduce 함수는 아래와 같이 동작해야 한다.
+log(add(add(add(add(add(0, 1), 2), 3), 4), 5)); // 15
+
+log(reduce(add, [1, 2, 3, 4, 5])); // 15
--- alstn2468/section3/src/7.js
+++ alstn2468/section3/src/7.js
@@ -1,2 +1,46 @@
 import 'regenerator-runtime/runtime';
+import { map, filter, reduce } from '../../lib/fx';
 import log from '../../lib/log';
+
+// map+filter+reduce 중첩 사용과 함수형 사고
+
+const products = [
+  { name: '반팔티', price: 15000 },
+  { name: '긴팔티', price: 20000 },
+  { name: '핸드폰케이스', price: 15000 },
+  { name: '후드티', price: 30000 },
+  { name: '바지', price: 25000 },
+];
+
+// map을 이용해 가격을 추출하는 로직
+log(map((p) => p.price, products)); // [15000, 20000, 15000, 30000, 25000]
+
+// 2만원 이하의 상품만 추출하는 로직
+log(
+  map(
+    (p) => p.price,
+    filter((p) => p.price < 20000, products)
+  )
+); // [15000, 15000]
+
+const add = (a, b) => a + b;
+
+// 2만원 이하의 상품의 가격 총합을 추출하는 로직
+log(
+  reduce(
+    add,
+    map(
+      (p) => p.price,
+      filter((p) => p.price < 20000, products)
+    )
+  )
+); // 30000
+log(
+  reduce(
+    add,
+    filter(
+      (n) => n < 20000,
+      map((p) => p.price, products)
+    )
+  )
+); // 30000
