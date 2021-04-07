4ed9687ec7669d119869f94aaf78f48fcac30131
Minsu Kim <alstn2468_@naver.com>
2021-04-07 19:14:46 +0900

    [minsu-210406] 퀴즈3 구현 추가 (#84)
    * [minsu-210406] MyPromise 구현 추가
    
    * 이터러블 구현 추가

--- alstn2468/quiz/3st-yesmoadcoder-test/src/index.js
+++ alstn2468/quiz/3st-yesmoadcoder-test/src/index.js
@@ -2,29 +2,82 @@
   커스텀 promise 구현하기
 */
 
-// const MyPromise = class {
-//   constructor(executor) {}
-//   resolve(value) {}
-//   then(callback) {}
-//   catch(callback) {}
-// };
+const PROMISE_STATUS = {
+  PENDING: 'pending',
+  RESOLVED: 'resolved',
+  REJECTED: 'rejected',
+};
 
-// new MyPromise((resolve, reject) => {
-//   setTimeout(() => {
-//     resolve("첫번째 프로미스");
-//   }, 1000);
-// })
-//   .then((value) => {
-//     console.log(value);
-//     return "두번째 프로미스";
-//   })
-//   .then((value) => {
-//     console.log(value);
-//			throw '이건 에러야'
-//   })
-//   .catch((e) => {
-//     console.log(e);
-//   });
+const MyPromise = class {
+  constructor(executor) {
+    this.status = PROMISE_STATUS.PENDING;
+    this.result = undefined;
+    this.queue = [];
+    try {
+      executor(
+        (value) => this.resolve(value),
+        (value) => this.reject(value)
+      );
+    } catch (error) {
+      this.reject(error);
+    }
+  }
+  resolve(value) {
+    if (this.status !== PROMISE_STATUS.PENDING) return this;
+    this.status = PROMISE_STATUS.RESOLVED;
+    this.result = value;
+    this.queue.forEach((resolve) => resolve());
+  }
+  reject(error) {
+    if (this.status !== PROMISE_STATUS.PENDING) return this;
+    this.status = PROMISE_STATUS.REJECTED;
+    this.result = error;
+    this.queue.forEach((reject) => reject());
+  }
+  then(callback) {
+    switch (this.status) {
+      case PROMISE_STATUS.PENDING:
+        return new MyPromise((resolve) => {
+          this.queue.push(() => resolve(callback(this.result)));
+        });
+      case PROMISE_STATUS.RESOLVED:
+        return new MyPromise((resolve) => resolve(callback(this.result)));
+      case PROMISE_STATUS.REJECTED:
+        break;
+    }
+    return this;
+  }
+  catch(callback) {
+    switch (this.status) {
+      case PROMISE_STATUS.PENDING:
+        return new MyPromise((reject) => {
+          this.queue.push(() => reject(callback(this.result)));
+        });
+      case PROMISE_STATUS.RESOLVED:
+        break;
+      case PROMISE_STATUS.REJECTED:
+        return new MyPromise((reject) => reject(callback(this.result)));
+    }
+    return this;
+  }
+};
+
+new MyPromise((resolve, reject) => {
+  setTimeout(() => {
+    resolve('첫번째 프로미스');
+  }, 1000);
+})
+  .then((value) => {
+    console.log(value);
+    return '두번째 프로미스';
+  })
+  .then((value) => {
+    console.log(value);
+    throw '이건 에러야';
+  })
+  .catch((e) => {
+    console.log(e);
+  });
 
 // 출력
 // 첫번째 프로미스
@@ -35,11 +88,25 @@
   커스텀 이터러블 이터레이터 구현하기
 */
 
-// const iterable = ;
+const iterable = {
+  [Symbol.iterator]() {
+    let i = 1;
+    return {
+      next() {
+        return i > 5
+          ? { value: undefined, done: true }
+          : { value: i++, done: false };
+      },
+      [Symbol.iterator]() {
+        return this;
+      },
+    };
+  },
+};
 
-// let iterator = ;
-// iterator.next();// 1
-// iterator.next();// 2
+let iterator = iterable[Symbol.iterator]();
+console.log(iterator.next()); // { value: 1, done: false }
+console.log(iterator.next()); // { value: 2, done: false }
 
-// for (const a of iterator) console.log(a); // 3 4 5
-// for (const a of iterable) console.log(a); // 1 2 3 4 5
+for (const a of iterator) console.log(a); // 3 4 5
+for (const a of iterable) console.log(a); // 1 2 3 4 5
