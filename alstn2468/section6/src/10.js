b6c12272e4412e87c92a7a65d244ca3749e69a87
alstn2468 <alstn2468_@naver.com>
2021-04-11 00:43:45 +0900

    [minsu-210411] section6 코드 초기화

---
+++ alstn2468/section6/src/10.js
@@ -0,0 +1,5 @@
+import 'regenerator-runtime/runtime';
+import log from '../../lib/log';
+import { go, reduce, map, curry } from '../../lib/fx';
+
+// map, filter 계열 함수들이 가지는 결합 법칙
---
+++ alstn2468/section6/src/8.js
@@ -0,0 +1,5 @@
+import 'regenerator-runtime/runtime';
+import log from '../../lib/log';
+import { go, reduce, map, curry } from '../../lib/fx';
+
+// L.range, L.map, L.filter, take 의 평가 순서
