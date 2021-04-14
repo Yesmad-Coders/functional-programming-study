506e3f6d20e342b817cc3808ed53637158bf4562
Minsu Kim <alstn2468_@naver.com>
2021-04-14 10:27:26 +0900

    [minsu-210413] section6 코드7 추가 (#89)

---
+++ alstn2468/lib/L.js
@@ -0,0 +1,20 @@
+import { curry } from './fx';
+
+const L = {};
+
+L.range = function* (l) {
+  let i = -1;
+  while (++i < l) yield i;
+};
+
+L.map = curry(function* (f, iter) {
+  for (const a of iter) yield f(a);
+});
+
+L.filter = curry(function* (f, iter) {
+  for (const a of iter) {
+    if (f(a)) yield a;
+  }
+});
+
+export default L;
