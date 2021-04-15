2fdac717b5d43477366f92534e4a69149ea5c484
alstn2468 <alstn2468_@naver.com>
2021-04-15 21:48:08 +0900

    [minsu-210415] section6 남은 부분 마무리

--- alstn2468/section6/src/10.js
+++ alstn2468/section6/src/10.js
@@ -1,5 +1,12 @@
 import 'regenerator-runtime/runtime';
+import L from '../../lib/L';
 import log from '../../lib/log';
-import { go, reduce, map, curry } from '../../lib/fx';
+import { go, map, filter, take, range } from '../../lib/fx';
 
 // map, filter 계열 함수들이 가지는 결합 법칙
+// - 사용하는 데이터가 무엇이든지
+// - 사용하는 보조 함수가 순수 함수라면 무엇이든지
+// - 아래와 같이 결합한다면 둘 다 결과가 같다.
+// [[mapping, mapping], [filtering, filtering], [mapping, mapping]]
+// =
+// [[mapping, filtering, mapping], [mapping, filtering, mapping]]
