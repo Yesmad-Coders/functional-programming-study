cb2b956fcc5d9a2f198112ce5cb3d4800ae14dc0
Minsu Kim <alstn2468_@naver.com>
2021-04-18 15:04:15 +0900

    [minsu-210417] section7 take, find함수 까지 코드 추가 (#95)

--- alstn2468/section7/src/7.js
+++ alstn2468/section7/src/7.js
@@ -2,3 +2,5 @@ import 'regenerator-runtime/runtime';
 import L from '../../lib/L';
 import log from '../../lib/log';
 import { go, map, filter, take, range } from '../../lib/fx';
+
+// yield *, L.deepFlat
